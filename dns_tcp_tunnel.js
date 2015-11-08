32392215767d564ff0a28b94d7888e38ef70f588
Bulent Vural <bulentv@gmail.com>
2015-11-08 22:15:48 +0200

    Target requested listen port, monitor over port 2121

--- dns_tcp_tunnel.js
+++ dns_tcp_tunnel.js
@@ -1,14 +1,18 @@
+
 var net = require('net');
 
 var appc = null;
 var dnsc = null;
 var apps = null;
-var dnss = null;
+var dnss = {};
 
 var dns = {};
 var app = {};
 var mode = null;
 var help = null;
+var id = 'default';
+var rport = null;
+var rapps = {};
 
 for(var i=2; i<process.argv.length; i++) {
   var argv = process.argv[i];
@@ -24,6 +28,16 @@ for(var i=2; i<process.argv.length; i++) {
     app = { host:parts[0], port:parseInt(parts[1]) || 53};
     continue;
   }
+  if(argv == '-i' || argv == '--id'){
+    i++;
+    id = process.argv[i];
+    continue;
+  }
+  if(argv == '-r' || argv == '--rport'){
+    i++;
+    rport = parseInt(process.argv[i]) || 222;
+    continue;
+  }
   if(argv == '-c' || argv == '--client') mode = 'c';
   if(argv == '-s' || argv == '--server') mode = 's';
   if(argv == '-h' || argv == '--help') test = true;
@@ -36,19 +50,22 @@ if(help || process.argv.length < 6) {
     "node dns_tcp_tunnel.js [-s|-c]  -d host:port -a host:port\n"+
     "\n"+
     "Options:\n"+
+    "-i, --id     : Identifier\n"+
     "-s, --server : Start in server mode\n"+
     "-c, --client : Start in server mode\n"+
     "-d, --dns    : DNS server or dns listen host:port\n"+
     "-a, --app    : Application server or listen host:port\n"+
+    "-r, --rport  : Remote port to request listen\n"+
     "-h, --help   : Print this info\n");
     
   process.exit(0);
 }
 
+console.log('Program started (id:'+id+')');
+
 switch(mode) {
   case 's':
     start_server();
-    start_app_server();
     break;
   case 'c':
     start_client();
@@ -57,37 +74,57 @@ switch(mode) {
     console.log("[s]erver or [c]lient?");
     break;
 }
+start_info_server();
 
 function start_server() {
   console.log('starting server..');
   var server = net.createServer( function(_dnss) {
     console.log('a client is accepted');
 
+    var rapp = {};
+
     _dnss.on('data', function(data) {
       console.log("dnss:received "+data.length+" bytes");
-      if(!dnss) {
-        dnss = _dnss;
-        console.log('Discarding first packet, sending first packet..');
-        console.log(data.toString("hex"));
-        _dnss.write(new Buffer('0000818000010001000000000c000c0001000100000000000420202020','hex'));
-
+      
+      var l = data.readUInt32BE(9);
+      if(l == 16 && data.length == 29) {
+        // client registration
+        var parts = data.slice(13).toString().split(/\//);
+        console.log("rapp = ",rapp);
+        rapp = {
+          id: parts[0],
+          port: parts[1],
+        }
+        rapps[rapp.id+'_'+rapp.port] = JSON.parse(JSON.stringify(rapp));
+        rapps[rapp.id+'_'+rapp.port].clients = {};
+        rapp.dnss = _dnss;
+        start_app_server(rapp);
+        rapp.dnss.write(new Buffer('0000818000010001000000000c000c0001000100000000000420202020','hex'));
       }else{
-        if(apps) {
-          apps.write(data);
+        if(rapp.apps) {
+          rapp.apps.write(data);
         }
       }
     });
 
     _dnss.on('end', function() {
-      dnss = null;
-      console.log('client disconnected');
+      rapp.dnss = null;
+      if(rapp.apps) {
+        rapp.apps.end();
+        rapp.apps = null;
+      }
+      if(rapp.server) {
+        rapp.server.close();
+      }
+      delete rapps[rapp.id+'_'+rapp.port];
+      rapp = null;
+      console.log('dnss client disconnected');
     });
   });
 
   server.on('error', function(e) {
     console.log('dnss has an error, retrying');
     server.close();
-    dnss = null;
     server.removeAllListeners();
     setTimeout( function() {
       start_server();
@@ -99,48 +136,85 @@ function start_server() {
   });
 }
 
-function start_app_server() {
+function start_app_server(rapp) {
   console.log('starting app server..');
   var server = net.createServer( function(_apps) {
-    console.log('an app client is accepted, ' + _apps.remoteAddress + ":" + _apps.remotePort);
-    apps = _apps;
+    console.log('an app client for ('+rapp.id+'#'+rapp.port+')is accepted, ' + _apps.remoteAddress + ":" + _apps.remotePort);
+    rapp.apps = _apps;
+
+    var client_id = _apps.remoteAddress+'_'+_apps.remotePort;
+    rapps[rapp.id+'_'+rapp.port].clients[client_id] = true;
 
     _apps.on('data', function(data) {
       console.log("apps:received "+data.length+" bytes");
-      if(dnss) {
-        dnss.write(data);
+      if(rapp.dnss) {
+        rapp.dnss.write(data);
       }
     });
 
     _apps.on('end', function() {
-      apps = null;
+      delete rapps[rapp.id+'_'+rapp.port].clients[client_id];
+      rapp.apps = null;
       console.log('app client disconnected');
-      if(dnss) {
-        dnss.write(new Buffer("313370","hex"));
+      if(rapp.dnss) {
+        rapp.dnss.write(new Buffer("313370","hex"));
       }
     });
   });
 
   server.on('error', function(e) {
-    console.log('apps has an error, retrying');
+    delete rapps[rapp.id+'_'+rapp.port].clients[_apps.remoteAddress+'_'+_apps.remotePort];
+    console.log('apps has an error, retrying',e);
     server.close();
-    apps = null;
+    rapp.apps = null;
+    rapp.server = null;
+    delete rapps[rapp.id+'_'+rapp.port];
     server.removeAllListeners();
-    setTimeout( function() {
-      start_app_server();
-    },1000);
   });
 
-  server.listen( app.port, app.host, function() {
-    console.log("server is listening (app) " + app.host + ":" + app.port);
+  server.listen( rapp.port, "0.0.0.0", function() {
+    console.log("server is listening (app) 0.0.0.0:" + rapp.port);
+    rapp.server = server;
   });
 }
+
+function start_info_server() {
+  console.log('starting info server..');
+  var server = net.createServer( function(_is) {
+    var report = JSON.stringify({
+      dns:dns,
+      app:app,
+      mode:mode,
+      id:id,
+      rport:rport,
+      rapps:rapps
+    },null,"\t");
+    _is.write(report+"\n");
+    _is.end();
+  });
+
+  server.on('error', function(e) {
+    console.log("error:",e);
+  });
+
+  server.listen( 2121, "0.0.0.0", function() {
+    console.log("info server is listening 0.0.0.0:2121");
+  });
+}
+
 
 function start_client() {
   console.log('starting client..');
   var _dnsc = net.connect( dns.port, dns.host, function() {
     console.log('dnsc connected');
-    _dnsc.write(new Buffer('0000010000010000000000000420202020','hex'));
+    var header = new Buffer('00000100000100000000000000','hex');
+    var idpadded = id+"/"+rport+"/";
+    while(idpadded.length<16) idpadded = idpadded + "*";
+    header.writeUInt32BE(idpadded.length, header.length - 4);
+    var buffer = new Buffer(header.length + idpadded.length);
+    header.copy(buffer);
+    buffer.write(idpadded,header.length);
+    _dnsc.write(buffer);
   });
   _dnsc.on('data', function(data) {
     console.log('dnsc recv ' + data.length + ' bytes');
@@ -151,6 +225,8 @@ function start_client() {
       return;
     }
     if(dnsc==null){
+      var l = data.readUInt32BE(9);
+      console.log(l);
       console.log("dnsc: discarding first packet");
       console.log(data.toString("hex"));
       dnsc = _dnsc;
